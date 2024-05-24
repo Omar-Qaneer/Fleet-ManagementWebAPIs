38eabf66dd37ab8cabd6b0df4951f8ac8b5d268c
OMAR <s11925044@stu.najah.edu>
2024-05-24 20:41:39 +0300

    Create Geofences Service and implement function to retrieve all Geofences data.

---
+++ Fleet ManagementWebApplication/Services/GeofencesService.cs
@@ -0,0 +1,20 @@
+﻿using Fleet_ManagementWebApplication.Models;
+
+namespace Fleet_ManagementWebApplication.Services
+{
+    public class GeofencesService : IGeofencesService
+    {
+        private readonly IDbService _dbService;
+
+        public GeofencesService(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public async Task<IEnumerable<Geofences>> GetGeofences()
+        {
+            var geofencesList = await _dbService.GetAll<Geofences>("SELECT * FROM Vehicles");
+            return geofencesList;
+        }
+    }
+}
---
+++ Fleet ManagementWebApplication/Services/IGeofencesService.cs
@@ -0,0 +1,9 @@
+﻿using Fleet_ManagementWebApplication.Models;
+
+namespace Fleet_ManagementWebApplication.Services
+{
+    public interface IGeofencesService
+    {
+        Task<IEnumerable<Geofences>> GetGeofences();
+    }
+}
