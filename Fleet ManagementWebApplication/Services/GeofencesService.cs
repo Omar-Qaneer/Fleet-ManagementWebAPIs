295aa54506891c2efeb497cd657064e3fe46cdd8
OMAR <s11925044@stu.najah.edu>
2024-05-25 09:45:52 +0300

    Create retrieval functions to get all  CircleGeofence, RectangleGeofence, and PolygonGeofence in Geofences Service.

--- Fleet ManagementWebApplication/Services/GeofencesService.cs
+++ Fleet ManagementWebApplication/Services/GeofencesService.cs
@@ -4,7 +4,7 @@ namespace Fleet_ManagementWebApplication.Services
 {
     public class GeofencesService : IGeofencesService
     {
-        private readonly IDbService _dbService;
+        private readonly IDbService _dbService; 
 
         public GeofencesService(IDbService dbService)
         {
@@ -13,8 +13,26 @@ namespace Fleet_ManagementWebApplication.Services
 
         public async Task<IEnumerable<Geofences>> GetGeofences()
         {
-            var geofencesList = await _dbService.GetAll<Geofences>("SELECT * FROM Vehicles");
+            var geofencesList = await _dbService.GetAll<Geofences>("SELECT * FROM Geofences");
             return geofencesList;
+        }
+
+        public async Task<IEnumerable<CircleGeofence>> GetCircleGeofence()
+        {
+            var circleGeofenceList = await _dbService.GetAll<CircleGeofence>("SELECT * FROM CircleGeofence");
+            return circleGeofenceList;
+        }
+
+        public async Task<IEnumerable<RectangleGeofence>> GetRectangleGeofence()
+        {
+            var rectangleGeofenceList = await _dbService.GetAll<RectangleGeofence>("SELECT * FROM RectangleGeofence");
+            return rectangleGeofenceList;
+        }
+
+        public async Task<IEnumerable<PolygonGeofence>> GetPolygonGeofence()
+        {
+            var polygonGeofenceList = await _dbService.GetAll<PolygonGeofence>("SELECT * FROM PolygonGeofence");
+            return polygonGeofenceList; 
         }
     }
 }
